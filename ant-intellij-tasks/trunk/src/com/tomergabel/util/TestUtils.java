e60f715217eb15f7bce8251c1709946243760051
Tomer <Tomer@599930c4-5d3a-11de-b59c-372aa07dcf7f>
2009-06-10 19:07:52 +0000

    * Updated project VCS root * Split test utility methods to TestUtils

---
+++ ant-intellij-tasks/trunk/src/com/tomergabel/util/TestUtils.java
@@ -0,0 +1,57 @@
+package com.tomergabel.util;
+
+import java.util.Collection;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public final class TestUtils {
+    public static <T> void assertSetEquality( Collection<T> expected, T[] actual ) {
+        assertSetEquality( null, expected, actual );
+    }
+
+    public static <T> void assertSetEquality( T[] expected, Collection<T> actual ) {
+        assertSetEquality( null, expected, actual );
+    }
+
+    public static <T> void assertSetEquality( T[] expected, T[] actual ) {
+        assertSetEquality( null, expected, actual );
+    }
+
+    public static <T> void assertSetEquality( String message, Collection<T> expected, T[] actual ) {
+        assertSetEquality( message, expected, Arrays.asList( actual ) );
+    }
+
+    public static <T> void assertSetEquality( String message, T[] expected, Collection<T> actual ) {
+        assertSetEquality( message, Arrays.asList( expected ), actual );
+    }
+
+    public static <T> void assertSetEquality( String message, T[] expected, T[] actual ) {
+        assertSetEquality( message, Arrays.asList( expected ), Arrays.asList( actual ) );
+    }
+
+    public static <T> void assertSetEquality( Collection<T> expected, Collection<T> actual ) {
+        assertSetEquality( null, expected, actual );
+    }
+
+    public static <T> void assertSetEquality( String message, Collection<T> expected, Collection<T> actual ) {
+        final HashSet<T> expectedSet = new HashSet<T>( expected );
+        final HashSet<T> actualSet = new HashSet<T>( actual );
+        expectedSet.removeAll( actual );
+        actualSet.removeAll( expected );
+        if ( expectedSet.size() == 0 && actualSet.size() == 0 )
+            return;
+
+        final StringBuilder sb = new StringBuilder();
+        if ( message != null )
+            sb.append( message ).append( Character.toChars( Character.LINE_SEPARATOR ) );
+        sb.append( "Assertion failed: " );
+        if ( expectedSet.size() > 0 ) {
+            sb.append( "missing " ).append( expectedSet );
+            if ( actualSet.size() > 0 )
+                sb.append( ", " );
+        }
+        if ( actualSet.size() > 0 )
+            sb.append( "unexpected " ).append( actualSet );
+        throw new AssertionError( sb.toString() );
+    }
+}
--- ant-intellij-tasks/trunk/test/com/tomergabel/build/intellij/ModuleFileParseTests.java
+++ ant-intellij-tasks/trunk/test/com/tomergabel/build/intellij/ModuleFileParseTests.java
@@ -1,65 +1,12 @@
 package com.tomergabel.build.intellij;
 
-import static org.junit.Assert.assertEquals;
+import static com.tomergabel.util.TestUtils.*;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
 
 public class ModuleFileParseTests {
-
-    private static <T> void assertSetEquality( Collection<T> expected, T[] actual ) {
-        assertSetEquality( null, expected, actual );
-    }
-
-    private static <T> void assertSetEquality( T[] expected, Collection<T> actual ) {
-        assertSetEquality( null, expected, actual );
-    }
-
-    private static <T> void assertSetEquality( T[] expected, T[] actual ) {
-        assertSetEquality( null, expected, actual );
-    }
-
-    private static <T> void assertSetEquality( String message, Collection<T> expected, T[] actual ) {
-        assertSetEquality( message, expected, Arrays.asList( actual ) );
-    }
-
-    private static <T> void assertSetEquality( String message, T[] expected, Collection<T> actual ) {
-        assertSetEquality( message, Arrays.asList( expected ), actual );
-    }
-
-    private static <T> void assertSetEquality( String message, T[] expected, T[] actual ) {
-        assertSetEquality( message, Arrays.asList( expected ), Arrays.asList( actual ) );
-    }
-
-    private static <T> void assertSetEquality( Collection<T> expected, Collection<T> actual ) {
-        assertSetEquality( null, expected, actual );
-    }
-
-    private static <T> void assertSetEquality( String message, Collection<T> expected, Collection<T> actual ) {
-        final HashSet<T> expectedSet = new HashSet<T>( expected );
-        final HashSet<T> actualSet = new HashSet<T>( actual );
-        expectedSet.removeAll( actual );
-        actualSet.removeAll( expected );
-        if ( expectedSet.size() == 0 && actualSet.size() == 0 )
-            return;
-
-        final StringBuilder sb = new StringBuilder();
-        if ( message != null )
-            sb.append( message ).append( Character.toChars( Character.LINE_SEPARATOR ) );
-        sb.append( "Assertion failed: " );
-        if ( expectedSet.size() > 0 ) {
-            sb.append( "missing " ).append( expectedSet );
-            if ( actualSet.size() > 0 )
-                sb.append( ", " );
-        }
-        if ( actualSet.size() > 0 )
-            sb.append( "unexpected " ).append( actualSet );
-        throw new AssertionError( sb.toString() );
-    }
-
     @Test
     public void testModuleFileParsing() throws Exception {
         final File resource = new File( this.getClass().getResource( "parsing-test.iml" ).getFile() );
